e89906eaaaef6ce7a00354019c69121e46568311
Jonas <jonas.gregory@outlook.com>
2024-11-07 08:34:21 -0600

    Initial Commit

---
+++ Morning Motivation/Morning_MotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morning_MotivationApp.swift
+//  Morning Motivation
+//
+//  Created by Jonas R. Gregory on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Morning_MotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
