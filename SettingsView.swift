1ac6879521ffafdfab16af188d4cbd9d0422c8fb
Luke Christopulos <lukechristopulos@gmail.com>
2024-11-07 08:46:10 -0600

    settings view

---
+++ SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Morning Motivation
+//
+//  Created by Luke R. Christopulos on 11/7/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
