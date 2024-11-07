809da800dee9aa64e9a9dbbeba8585d3a469327f
Luke Christopulos <lukechristopulos@gmail.com>
2024-11-07 09:29:55 -0600

    cool stuff

--- Morning Motivation/SettingsView.swift
+++ Morning Motivation/SettingsView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var isOn = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            RoundedRectangle(cornerRadius: 65)
+                .stroke(Color.cyan, lineWidth: 15)
+                .ignoresSafeArea()
+                .background(
+                    RoundedRectangle(cornerRadius: 60)
+                        .fill(Color.clear)
+                        .ignoresSafeArea()
+                    
+                )
+                .shadow(color: Color.blue.opacity(1), radius: 15, x: 0, y: 0)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Settings")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                Spacer()
+                Toggle("Task List", systemImage: isOn ? "square.stack.3d.up.fill" : "square.stack.3d.up.slash.fill", isOn: $isOn)
+                    .font(.largeTitle)
+                    .tint(.blue)
+                    .toggleStyle(.button)
+                    .contentTransition(.symbolEffect)
+                    .padding()
+                
+                VStack {
+                    Spacer()
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     SettingsView()
