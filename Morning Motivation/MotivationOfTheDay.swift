f385d5e5f7185e1375329dd74d477a30b6fc4464
Jonas <jonas.gregory@outlook.com>
2024-11-07 08:43:58 -0600

    l

---
+++ Morning Motivation/MotivationOfTheDay.swift
@@ -0,0 +1,18 @@
+//
+//  MotivationOfTheDay.swift
+//  Morning Motivation
+//
+//  Created by Jonas R. Gregory on 11/7/24.
+//
+
+import SwiftUI
+
+struct MotivationOfTheDay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotivationOfTheDay()
+}
