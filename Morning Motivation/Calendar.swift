ad370c1db6b210fe8693270966045799a97a479b
Luke Christopulos <lukechristopulos@gmail.com>
2024-11-11 08:39:09 -0600

    calendar created

---
+++ Morning Motivation/Calendar.swift
@@ -0,0 +1,18 @@
+//
+//  Calendar.swift
+//  Morning Motivation
+//
+//  Created by Luke R. Christopulos on 11/11/24.
+//
+
+import SwiftUI
+
+struct Calendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Calendar()
+}
