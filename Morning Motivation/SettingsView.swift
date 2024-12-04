866299a287d94d149f2705b3934e215d03d2a1e5
Jackson Laesch <jlaesch7219@stu.d214.org>
2024-12-04 08:41:25 -0600

    dsa

--- Morning Motivation/SettingsView.swift
+++ Morning Motivation/SettingsView.swift
@@ -12,72 +12,71 @@ struct SettingsView: View {
     @AppStorage("OutlineOn") var OutlineOn = true
     @AppStorage("ColorScheme") var ColorScheme = false
     @AppStorage("Submit") var Submit = true
+    @AppStorage("Notifications") var Notifications = false
     @State var showingAlert = false
     @State var newQuote: String = ""
     var body: some View {
-            ZStack{
-                if OutlineOn == true {
-                    RoundedRectangle(cornerRadius: 65)
-                        .stroke(Color.cyan, lineWidth: 15)
-                        .ignoresSafeArea()
-                        .background(
-                            RoundedRectangle(cornerRadius: 65)
-                                .fill(Color.clear)
-                                .ignoresSafeArea()
-                        )
-                        .shadow(color: Color.blue.opacity(1), radius: 15, x: 0, y: 0)
-                        .ignoresSafeArea()
-                }
-                VStack {
-                    Text("Settings")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding(35)
-                    Spacer()
-                    Toggle("Submit a Quote", systemImage: isOn ? "tree" : "square.stack.3d.up.fill", isOn: $showingAlert)
+        ZStack{
+            if OutlineOn == true {
+                RoundedRectangle(cornerRadius: 65)
+                    .stroke(Color.cyan, lineWidth: 15)
+                    .ignoresSafeArea()
+                    .background(
+                        RoundedRectangle(cornerRadius: 65)
+                            .fill(Color.clear)
+                            .ignoresSafeArea()
+                    )
+                    .shadow(color: Color.blue.opacity(1), radius: 15, x: 0, y: 0)
+                    .ignoresSafeArea()
+            }
+            VStack{ NavigationView{
+                Form{
+                    Section(header: Text("Display"),
+                            footer: Text("Dark mode will override system preferences.")){
+                        
+                        Toggle("Enhanced Look", systemImage: OutlineOn ? "livephoto" : "livephoto.slash", isOn: $OutlineOn)
+                            .tint(.blue)
+
+                           
+                        Toggle("Dark Mode", systemImage: ColorScheme ? "lightbulb" : "lightbulb.slash", isOn: $ColorScheme)
+                            .tint(.blue)
+                        
+                    }
+                    Section(header: Text("Features"),
+                            footer: Text("Turning on notifications allows us to send you reminders everday when the next motivation is available.")){
+                        Toggle("Task List", systemImage: isOn ? "square.stack.3d.up.fill" : "square.stack.3d.up.slash.fill", isOn: $isOn)
+                            .tint(.blue)
+                        Toggle("Notifications", systemImage: Notifications ? "bell" : "bell.slash", isOn: $Notifications)
+                            .tint(.blue)
+                        
+                        
+                    }
+                    Section {
+                        Button(action: {
+                            submitAQuote()
+                            showingAlert.toggle()
+                        }, label: {
+                            HStack{
+                                Image(systemName: "link")
+                                Text("Submit a Quote")
+                            }
+                        })
                         .alert("Submit Your Own Quote", isPresented: $showingAlert) {
                             TextField("type quote here...", text: $newQuote)
-                               
-                                                   .font(.custom("Arial", size: 16))
+                            
+                                .font(.custom("Arial", size: 16))
                                 .frame(width: 50, height: 30)
                             Button("OK", action: submitAQuote)
-                                   Button("Cancel", role: .cancel) { }
-                               }
-                        .font(.largeTitle)
-                        .tint(.blue)
-                        .toggleStyle(.button)
-                        .contentTransition(.symbolEffect)
-                        .frame(alignment: .leading)
-                        .padding()
-                        Spacer()
-                            .frame(height: 25)
-                Toggle("Task List", systemImage: isOn ? "square.stack.3d.up.fill" : "square.stack.3d.up.slash.fill", isOn: $isOn)
-                    .font(.largeTitle)
-                    .tint(.blue)
-                    .toggleStyle(.button)
-                    .contentTransition(.symbolEffect)
-                    .frame(alignment: .leading)
-                    .padding()
-                    Spacer()
-                        .frame(height: 35)
-                Toggle("Enhanced Look", systemImage: OutlineOn ? "livephoto" : "livephoto.slash", isOn: $OutlineOn)
-                    .font(.largeTitle)
-                    .tint(.blue)
-                    .toggleStyle(.button)
-                    .contentTransition(.symbolEffect)
-                    .frame(alignment: .leading)
-                    Spacer()
-                        .frame(height: 30)
-                Toggle("Dark Mode", systemImage: ColorScheme ? "lightbulb.fill" : "lightbulb.slash", isOn: $ColorScheme)
-                    .font(.largeTitle)
-                    .tint(.blue)
-                    .toggleStyle(.button)
-                    .contentTransition(.symbolEffect)
-                    .padding()
-                    Spacer()
-                        .frame(height: 0)
-                    
-                   
+                            Button("Cancel", role: .cancel) { }
+                        }
+                        
+                    }
+                    .foregroundColor(ColorScheme ? .white : .black)
+                    .font(.system(size: 16, weight: .semibold))
+                }
+                
+                .navigationTitle("Settings")
+            }
             }
         }
     }
@@ -85,6 +84,7 @@ struct SettingsView: View {
         
     }
 }
+
 #Preview {
     ContentView()
 }
